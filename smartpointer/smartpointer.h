0eddd487683b41bf1bd26dea9d5167e74dc136b8
liushuo <liushuo0826@sina.com>
2018-11-19 10:11:24 +0800

    Add reference count.

--- smartpointer/smartpointer.h
+++ smartpointer/smartpointer.h
@@ -1,25 +1,66 @@
 #ifndef SMART_POINTER_H_
 #define SMART_POINTER_H_
 #include <iostream>
+
+class RefBase{ //引用计数基类
+public:
+	RefBase() : mCount(0){}
+	void incRefCount() {
+		mCount++;
+	}
+	int decRefCount() {
+		return --mCount;
+	}
+	int getRefCount() {
+		return mCount;
+	}
+	virtual ~RefBase(){};
+private:
+	int mCount;
+};
 using namespace std;
 
 template <typename T>
 class SmartPointer {
 public:
 	SmartPointer():mPointer(NULL) {
-		cout << "Create unknown smart pointer" << endl;	
+		cout << "Create unknown smart pointer" << endl;
 	}
 
 	SmartPointer(T *p):mPointer(p) {
 		cout << "Create smart pointer at "<< \
 		static_cast<const void*>(p) << endl;
+		if(mPointer)
+			mPointer->incRefCount();
 	}
 	
 	~SmartPointer() {
 		cout << "Release smart pointer at "<< \
 		static_cast<const void*>(mPointer) << endl;
+		if(mPointer && mPointer->decRefCount()==0)
+			delete mPointer;
+	}
+	
+	//拷贝构造函数
+	SmartPointer(const SmartPointer &other):mPointer(other.mPointer) {
+		cout << "Copy constructor" << endl;
+		if(mPointer)
+			mPointer->incRefCount();
 	}
 
+	//赋值操作符
+	SmartPointer &operator = (const SmartPointer &other) {
+		T *temp(other.mPointer);
+		// 新指向对象，引用计数值加1
+		if(temp)
+			temp->incRefCount();
+		// 原指向对象，引用计数值减1，如果减1后引用计数为0 销毁原资源对象
+		if(mPointer && mPointer->decRefCount() == 0)
+			delete mPointer;
+		// 智能指针指向新资源对象
+		mPointer = temp;
+		return *this;
+	}
 private:
 	T *mPointer;
 };
--- smartpointer/sptest1.c
+++ smartpointer/sptest1.c
@@ -1,22 +1,50 @@
 #include <iostream>
 #include "smartpointer.h"
 
-class SomeClass {
+class SomeClass: public RefBase {
 public:
 	SomeClass() {
-		cout << "SomeClass constructor" << endl;	
+		cout << "SomeClass constructor" << endl;
+	}
+	~SomeClass() {
+		cout << "SomeClass deconstructor" << endl;
 	}
 };
 
-void testcase1() {
+class OtherClass: public RefBase {
+public:
+	OtherClass() {
+		cout << "OtherClass constructor" << endl;
+	}
+	~OtherClass() {
+		cout << "OtherClass deconstructor" << endl;
+	}
+};
+
+/*void testcase1() {
 	SmartPointer<char> spunknown;
 	SmartPointer<char> spnull = NULL;
 	SmartPointer<SomeClass> spclass = new SomeClass;
 	SmartPointer<const char> spstr = "Hello";
+}*/
+
+void testcase3() {
+	SomeClass *pSomeClass = new SomeClass();
+	SmartPointer<SomeClass> spOuter = pSomeClass;
+	cout << "SomeClass Ref Count:" << pSomeClass->getRefCount() << " outer.1"<< endl;
+	{
+		SmartPointer<SomeClass> spInner = spOuter;
+		cout << "SomeClass Ref Count:" << pSomeClass->getRefCount() << " inner." << endl;
+	}
+	cout << "SomeClass Ref Count:" << pSomeClass->getRefCount() << " outer.2"<< endl;
+
+	cout << "New another someclass for spOuter:" << endl;
+	SmartPointer<SomeClass> spOuter2 = new SomeClass();
+	spOuter = spOuter2;
 }
 
 int main(void)
 {
-	testcase1();
+	testcase3();
 	return 0;
 }
